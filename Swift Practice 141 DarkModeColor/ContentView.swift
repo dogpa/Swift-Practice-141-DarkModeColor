7969200c5cd64f9ccb763e763773c7fa5e0b2039
Dogpa <dogpa0130@gmail.com>
2022-03-29 21:30:07 +0800

    Initial Commit

---
+++ Swift Practice 141 DarkModeColor/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Swift Practice 141 DarkModeColor
+//
+//  Created by Dogpa's MBAir M1 on 2022/3/29.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Swift Practice 141 DarkModeColor/Swift_Practice_141_DarkModeColorApp.swift
@@ -0,0 +1,17 @@
+//
+//  Swift_Practice_141_DarkModeColorApp.swift
+//  Swift Practice 141 DarkModeColor
+//
+//  Created by Dogpa's MBAir M1 on 2022/3/29.
+//
+
+import SwiftUI
+
+@main
+struct Swift_Practice_141_DarkModeColorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
